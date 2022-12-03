54dab7284ce54decec514abe0b07ceaa7b87e0d3
Emma Boehly <emma.boehly@epfl.ch>
2022-12-03 23:18:12 +0100

    clean framework

--- datasets_generator.py
+++ datasets_generator.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def dataset_generator() :
+def datasets_generator() :
     dataset_sizes = np.logspace(0,6,7)
 
     Xs_train = [np.random.randn(nb_sample, nb_pred) for nb_sample, nb_pred in zip(dataset_sizes, dataset_sizes)]
-    ys_train = [np.random.randn(nb_sample) for nb_sample in dataset_sizes]
+    ys_train = [np.random.randint(0,2,nb_sample) for nb_sample in dataset_sizes]
     return Xs_train, ys_train
