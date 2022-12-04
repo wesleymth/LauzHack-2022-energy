f30df2e625d3275b5399b0f1c920867034125db9
Emma Boehly <emma.boehly@epfl.ch>
2022-12-04 11:59:32 +0100

    vfdrheh

---
+++ predictions.py
@@ -0,0 +1,21 @@
+from dataset_generator import *
+from joblib import dump, load
+from data_processing import rename_categorical_cols
+from train_model import one_hot_encoding_test
+
+def predict(df):
+    model = load('xgboost.joblib')
+    return model.predict(df)
+
+def energy(model, X):
+    dall = {}
+    for d in [get_cpu_features(), get_system_features()]:
+        dall.update(d)
+    dataset = pd.DataFrame([],columns=list(dall.keys()) + ["model_name", "nb_samples", "nb_preds"])
+    dataset.loc[len(dataset)] = list(dall.values()) + [model.__name__, X.shape[0], X.shape[1]]
+    #predictors = one_hot_encoding(dataset)
+    #predict(predictors)
+    onehot = load('one_hot_encoder.joblib')
+    transformed = onehot.fit_transform(dataset)
+    one_hot_df = pd.DataFrame(transformed.todense(), columns=onehot.get_feature_names_out())
+    return predict(one_hot_df)
--- train_model.py
+++ train_model.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import make_column_transformer
-from sklearn.linear_model import RidgeCV
+from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import GradientBoostingRegressor
 from joblib import dump
 from data_processing import rename_categorical_cols
+import numpy as np
 
 
 def train_models(dataset = 'energy_dataset.csv') :
@@ -15,19 +16,28 @@ def train_models(dataset = 'energy_dataset.csv') :
 
     one_hot_df = one_hot_encoding(dataset_X)
 
-    #Linear Regression Method with Ridge Cross Validation
-    ridge = RidgeCV().fit(one_hot_df, costs)
+    #Linear Regression Method with logarithmization of the costs
+    ridge = LinearRegression().fit(one_hot_df, np.log(costs))
 
     #XGBoost
     xgboost = GradientBoostingRegressor(random_state=0).fit(one_hot_df, costs)
-    dump(ridge, 'ridge.joblib')
-    dump(xgboost, 'xgboost.joblib')
+    dump(ridge, 'linear.joblib')
+    dump(xgboost, 'model.joblib')
     return ridge, xgboost
 
+"""def one_hot_encoding_test(dataset) :
+    dataset = rename_categorical_cols(dataset)
+    category_col = dataset.columns.str.contains('name')
+    one_hot_encoder = make_column_transformer((OneHotEncoder(drop ='first'), category_col), remainder='passthrough')
+    transformed = one_hot_encoder.fit_transform(dataset)
+    one_hot_df = pd.DataFrame(transformed, columns=one_hot_encoder.get_feature_names_out())
+    return one_hot_df, one_hot_encoder
+"""
 def one_hot_encoding(dataset) :
     #dataset = rename_categorical_cols(dataset)
     category_col = dataset.columns.str.contains('name')
     one_hot_encoder = make_column_transformer((OneHotEncoder(drop ='first'), category_col), remainder='passthrough')
     transformed = one_hot_encoder.fit_transform(dataset)
-    one_hot_df = pd.DataFrame(transformed, columns=one_hot_encoder.get_feature_names_out())
-    return one_hot_df
+    one_hot_df = pd.DataFrame(transformed.todense(), columns=one_hot_encoder.get_feature_names_out())
+    dump(one_hot_encoder, 'one_hot_encoder.joblib')
+    return one_hot_df, one_hot_encoder
