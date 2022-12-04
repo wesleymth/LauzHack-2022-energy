927af11fabea2e299e59f613d4cf7b80229a1cd5
Emma Boehly <emma.boehly@epfl.ch>
2022-12-04 12:19:46 +0100

    hzht

--- predictions.py
+++ predictions.py
@@ -12,10 +12,12 @@ def energy(model, X):
     for d in [get_cpu_features(), get_system_features()]:
         dall.update(d)
     dataset = pd.DataFrame([],columns=list(dall.keys()) + ["model_name", "nb_samples", "nb_preds"])
+    
     dataset.loc[len(dataset)] = list(dall.values()) + [model.__name__, X.shape[0], X.shape[1]]
     #predictors = one_hot_encoding(dataset)
     #predict(predictors)
     onehot = load('one_hot_encoder.joblib')
     transformed = onehot.fit_transform(dataset)
-    one_hot_df = pd.DataFrame(transformed.todense(), columns=onehot.get_feature_names_out())
+    one_hot_df = pd.DataFrame(transformed, columns=onehot.get_feature_names_out())
+    print(transformed)
     return predict(one_hot_df)
