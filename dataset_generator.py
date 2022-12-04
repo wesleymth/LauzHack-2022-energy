85eb8331a04f4b224f40a50aa1be63c08cbed97d
wesleymth <wesley.monteith-finas@epfl.ch>
2022-12-04 02:31:10 +0100

    debugging data generation

--- dataset_generator.py
+++ dataset_generator.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 #from dataset_initiator import dataset_initiator
 from energy_extractor_intel import *
 from sklearn import linear_model
+from sklearn.linear_model import LinearRegression, Ridge
 import GPUtil
 import psutil
 import platform
@@ -72,27 +73,28 @@ def dataset_generator(nb_dataset_=5, all_sklearn=False) :
     if all_sklearn :
         models = getmembers(skl.svm) + getmembers(skl.linear_model) + getmembers(skl.tree) + getmembers(skl.neural_network) + getmembers(skl.preprocessing) + getmembers(skl.ensemble)
     else : 
-        models = getmembers(linear_model)
+        #models = getmembers(linear_model)
+        models = [LinearRegression, Ridge]
 
-    for i, f in tqdm(enumerate(models)):
+    for i, f in enumerate(models):
         try:
-            if(f[0][0]!='_'):
-                for X, y in zip(Xs_train, ys_train):
-                    #fit model and record energy consumption
-                    IPG = IntelPowerGadget(duration=2,
-                                        resolution=1000,
-                                        #output_dir ='logs',
-                                        log_file_name='log_file.csv')
-                                    
-                    IPG._setup_cli()
-                    f[1]().fit(X,y)
-                    IPG._log_values()
-                    
-                    dataset.loc[i] = list(dall.values()) + [f.__name__, X.shape[0], X.shape[1], extract_energy('log_file.csv')]
-                    
-                    
-                    #rest for the processor to avoid successiv computation that could biased the energy consumption measurements
-                    time.sleep(2)
+            for X, y in zip(Xs_train, ys_train):
+                #fit model and record energy consumption
+                IPG = IntelPowerGadget(duration=2,
+                                    resolution=1000,
+                                    #output_dir ='logs',
+                                    log_file_name='log_file.csv')
+                                
+                IPG._setup_cli()
+                #f[1]().fit(X,y)
+                f().fit(X,y)
+                IPG._log_values()
+                
+                dataset.loc[i] = list(dall.values()) + [f.__name__, X.shape[0], X.shape[1], extract_energy('log_file.csv')]
+                
+                
+                #rest for the processor to avoid successiv computation that could biased the energy consumption measurements
+                time.sleep(2)
         except:
             #extract the model for which the train didn't work (issues with the parameter in general)
             non_working_models.append(f)
@@ -100,10 +102,14 @@ def dataset_generator(nb_dataset_=5, all_sklearn=False) :
     return dataset
 
 def train_data_generator(D_sup_N=True, nb_dataset=5) :
-    dataset_sizes = np.around(np.logspace(1,nb_dataset,nb_dataset)).astype(int)
+    #dataset_sizes = np.around(np.logspace(1,nb_dataset,nb_dataset)).astype(int)
+    dataset_sizes = [2,3]
     if D_sup_N :
         Xs_train = [np.random.randn(nb_sample, nb_pred) for nb_sample, nb_pred in zip(dataset_sizes, dataset_sizes)]
     else : 
         Xs_train = [np.random.randn(nb_sample, nb_pred) for nb_sample, nb_pred in zip(dataset_sizes, dataset_sizes) if nb_pred <= nb_sample]
     ys_train = [np.random.randint(0,2,nb_sample) for nb_sample in dataset_sizes]
     return Xs_train, ys_train
+
+
+data = dataset_generator(nb_dataset_=2)
